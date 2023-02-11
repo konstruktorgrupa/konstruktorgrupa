e61da16ccd7a7846173ebb94a48f862ebc062887
jani <learning.indie@gmail.com>
2023-02-11 19:51:10 +0800

    add deploy workflow

--- src/comp/Slika.jsx
+++ src/comp/Slika.jsx
@@ -1,22 +1,23 @@
-import { useState } from 'react'
-import SlikaFocus from './SlikaFocus'
+import { useState } from "react";
+import SlikaFocus from "./SlikaFocus";
 
 function Slika(props) {
-
-    function ImageBig() {
-document.getElementById('slikaFocus').style.display = 'block'
-document.getElementById('menu').style.display = 'none'
-document.getElementById('imgSrc').src = props.img
-    }
+  function ImageBig() {
+    document.getElementById("slikaFocus").style.display = "block";
+    document.getElementById("menu").style.display = "none";
+    document.getElementById("imgSrc").src = props.img;
+  }
 
   return (
     <div>
-                   <SlikaFocus />
-          <img style={{ width: props.screenWidth/4}}
-          src={props.img} onClick={ImageBig}/>
-
+      <SlikaFocus />
+      <img
+        style={{ width: props.screenWidth / 5 }}
+        src={props.img}
+        onClick={ImageBig}
+      />
     </div>
-  )
+  );
 }
 
-export default Slika
+export default Slika;
