2e4d90bec42a9842380331dc522dc701ea731561
jani <learning.indie@gmail.com>
2023-02-23 18:14:14 +0800

    add deploy workflow

--- src/pages/ONama.jsx
+++ src/pages/ONama.jsx
@@ -29,21 +29,43 @@ function ONama(props) {
           zIndex: "10 !important",
           marginLeft: "auto",
           marginRight: "auto",
+          fontSize: "0.7vmin",
         }}
       >
         <h1>
-          Naša firma se specijalizira za uvođenje instalacija vodovoda,
-          kanalizacije, plina te instalacija za grijanje i klimatizaciju.
+          Mi se ponosimo našom dugogodišnjom tradicijom i iskustvom u radu sa
+          svim vrstama instalacija. Naši stručnjaci su osposobljeni za
+          obavljanje svih vrsta radova, od manjih popravaka do većih projekata
+          koji uključuju postavljanje kompletnih sustava u novim ili postojećim
+          objektima.
           <br></br>
-          Naš stručni tim pruža kvalitetne usluge, a naši iskusni stručnjaci
-          koriste najnoviju tehnologiju i materijale kako bi osigurali sigurnost
-          i pouzdanost vaših instalacija.
+          <br></br>
+          Kada radimo na vašem projektu, koristimo najbolje materijale i alate
+          kako bismo osigurali dugotrajnost i pouzdanost vaših instalacija.
+          Svaki projekt pristupamo individualno, pa su svi naši radovi
+          prilagođeni vašim potrebama i željama.
           <br></br> <br></br>
-          Pružamo usluge za različite vrste objekata, bilo da se radi o
-          privatnim kućama, poslovnim prostorima ili industrijskim objektima.
+          Naša usluga ne staje nakon instalacije sustava, jer vam također nudimo
+          redovito održavanje i servisiranje vaših sustava kako bi osigurali
+          njihovu optimalnu funkcionalnost i pouzdanost. Naši stručnjaci će brzo
+          i efikasno otkloniti sve probleme ili kvarove koji se mogu pojaviti.
+          <br></br> <br></br>
+          Kao vodeći stručnjaci u našoj industriji, uvijek smo u koraku s
+          najnovijim trendovima i tehnologijama u industriji. Naša stručnost i
+          iskustvo omogućuju nam da brzo i efikasno riješimo sve vaše probleme i
+          odgovorimo na sve vaše potrebe.
+          <br></br> <br></br>
+          Naša misija je pružiti klijentima najbolje moguće rješenje za njihove
+          potrebe uz najbolju vrijednost za njihov novac. Stalno radimo na
+          poboljšanju i unaprjeđenju naših usluga kako bi se osigurala najbolja
+          moguća kvaliteta za naše klijente.
           <br></br>
-          Naš cilj je osigurati da vaši sustavi rade učinkovito i pouzdano kako
-          bi vaša svakodnevica bila ugodna i udobna.
+          <br></br>
+          Ako imate bilo kakvih pitanja ili trebate bilo kakve usluge u vezi s
+          instalacijama, slobodno nas kontaktirajte. Naši stručnjaci će vam
+          pružiti potrebne informacije i pomoći vam pronaći rješenje koje
+          najbolje odgovara vašim potrebama.
+          <br></br>
           <br></br>
           <br></br>
           RADNO VRIJEME
